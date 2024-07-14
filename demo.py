00ac8f54ea61d59acc1b31760b0eaf27a05c5550
SullivanC19 <colinsullivan203@gmail.com>
2024-07-14 13:38:50 -0700

    loop for demo

--- demo.py
+++ demo.py
@@ -1,22 +1,39 @@
 import djitellopy as tello
+import time
+import cv2
 
 node = tello.Tello()
 
 node.connect()
+print(node.get_battery())
 
 # take off and move to ceiling
 node.takeoff()
-node.move_up(100)
+# node.move_up(100)
 
-node.set_video_direction(tello.Tello.CAMERA_DOWNWARD)
-
-frames = node.get_frame_read(with_queue=True)
+node.streamon()
+node.send_command_with_return("downvision 1")
+frame = node.get_frame_read()
 
 # display frames from drone
-for img in frames:
-  pass
+while True:
+    img = frame.frame
 
+    cv2.imshow('win', img)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+
+  # 1. detect person bounding boxes and select the one closest to the center
+
+
+  # 2. move to center bounding box
 
 node.land()
+
+cv2.destroyAllWindows()
+
+# node.land()
 
 node.end()
